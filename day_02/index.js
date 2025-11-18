95c5ee7c5853adacf37502951c54eb869bee7dbf
sailesh-010 <saileshshahi2205bit@balmikilincoln.edu.np>
2025-11-18 20:11:38 +0545

    Update day_02

---
+++ day_02/index.js
@@ -0,0 +1,13 @@
+let name = "sailesh";
+let age = 20;
+let married = null;
+let isStudent = true;
+let userData;
+userData = undefined;
+
+
+console.log("Name:", name + " and typeof name is " + typeof name);
+console.log("Age:", age + " and typeof age is " + typeof age);
+console.log("Married:", married + " and typeof married is " + typeof married);
+console.log("Is Student:", isStudent + " and typeof isStudent is " + typeof isStudent);
+console.log("User Data:", userData + " and typeof userData is " + typeof userData);
