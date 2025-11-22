31782bb10984cafe108303e4cbca2a3ffff51db3
sailesh-010 <saileshshahi2205bit@balmikilincoln.edu.np>
2025-11-22 20:21:26 +0545

    Add day_05: Learing about For-loop and while-loop in js

---
+++ day_06/index.js
@@ -0,0 +1,13 @@
+let i = 0;
+for (i=0; i<=8; i+=2){
+     console.log("hello, world! i am inside a for loop and its "+ i/2 +" time(s)!")
+     if (i === 8){
+        break;
+     }
+}
+
+let j = 0;
+while (j <= 8){
+    console.log("hello, world! i am inside a while loop and its "+ j/2 +" time(s)!");
+    j+=2;
+}
