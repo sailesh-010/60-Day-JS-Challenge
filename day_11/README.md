c3a2d79809acd49c05b2e3fb4bdfc2a3fc66f3e3
sailesh-010 <saileshshahi2205bit@balmikilincoln.edu.np>
2025-11-27 19:31:31 +0545

    Add day_11: Learning about arrays in JS

---
+++ day_11/README.md
@@ -0,0 +1,8 @@
+This is my day 11 of JS for 60 days challenge.
+
+## overview
+    In this lesson, we will learn about arrays in JavaScript. Arrays are a fundamental part of JavaScript and are used to store collections of data.
+
+## Today I learn about:
+    -arrays
+    -methods in arrays
---
+++ day_11/index.js
@@ -0,0 +1,12 @@
+let numbers = [1, 2, 3, 4, 5];
+console.log("Original array:", numbers);
+
+
+let addnumbers = numbers.push(6);
+console.log("Array after push:", numbers);
+
+let remnumbers = numbers.pop();
+console.log("Array after pop:", numbers);
+console.log("Reversed array:", numbers.reverse());
+
+console.log("length:", numbers.length);
